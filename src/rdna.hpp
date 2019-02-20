605c8d6bd6d93476cfc53794af48512f32006998
storm750 <storm750@users.noreply.github.com>
2019-02-19 21:08:47 -0400

    added in a condition which covers if the user enters an empty string

--- src/rdna.hpp
+++ src/rdna.hpp
@@ -10,12 +10,18 @@ string randDNA(int seed,string bases,int n)
 	int max = bases.size() - 1; //Max input of letters inputted by user - 1 to get correct index.
 	string letters = ""; //Variable to hold the indices of the chosen letters, additively.
 	
+	//condition if users enters an empty string.
+	if (bases == "")
+	{
+		return "";
+	}
+	
 	mt19937 eng1 (seed); //user inputs the seed.
 	uniform_int_distribution <> uni(min,max); //uniform distribution of int type.
 	
 	for (int i = 0; i < n; i++) //Loop going until limit set by the user for the amount of random sequences to generate.
 	{
-		index = uni(eng1);
+		index = uni(eng1);//made a change here
 		letters += bases[index];
 	}
 	
